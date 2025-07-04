21279064783fc6da34caf9bd9dcc9907916c6659
unknown <rakshat2001@gmail.com>
2025-07-04 22:19:28 +0530

    created users table did alemnbic migrations

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Numeric
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey, DateTime, Numeric
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from app.db import Base
@@ -29,3 +29,21 @@ class PhoneModel(Base):
     updated_at = Column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now())
 
     brand = relationship("PhoneBrand", back_populates="models")
+
+
+class UserModel(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, nullable=False, unique=True)
+    name = Column(String, nullable=False)
+    password = Column(String, nullable=True)
+
+    auth_provider = Column(String, default="local")
+    provider_user_id = Column(String, nullable=True) 
+
+    is_active = Column(Boolean, nullable=False, default=True)
+    last_login = Column(DateTime(timezone=True), nullable=True)
+    
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now())
