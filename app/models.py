821dff7fb2a8ac4b1bd2f44535ceeedcf2fe71a0
unknown <rakshat2001@gmail.com>
2025-07-09 09:33:50 +0530

    made changes in db schema of user and phone tables for better security

--- app/models.py
+++ app/models.py
@@ -1,12 +1,15 @@
-from sqlalchemy import Boolean, Column, Integer, String, ForeignKey, DateTime, Numeric
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey, DateTime, Numeric, UUID
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
+from sqlalchemy import Enum
 from app.db import Base
+from uuid import uuid4
+import enum
 
 class PhoneBrand(Base):
     __tablename__ = "phone_brands"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid4)
     name = Column(String, unique=True, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now())
@@ -17,9 +20,9 @@ class PhoneBrand(Base):
 class PhoneModel(Base):
     __tablename__ = "phone_models"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     name = Column(String, nullable=False)
-    brand_id = Column(Integer, ForeignKey("phone_brands.id"), nullable=False)
+    brand_id = Column(UUID(as_uuid=True), ForeignKey("phone_brands.id"), nullable=False, index=True)
 
     phone_width = Column(Numeric(5, 2))  
     phone_height = Column(Numeric(5, 2)) 
@@ -31,15 +34,22 @@ class PhoneModel(Base):
     brand = relationship("PhoneBrand", back_populates="models")
 
 
+class AuthProvider(enum.Enum):
+    local = "local"
+    google = "google"
+    github = "github"
+
+
 class UserModel(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    public_id = Column(UUID(as_uuid=True),  unique=True, default=uuid4)
     email = Column(String, nullable=False, unique=True)
     name = Column(String, nullable=False)
     password = Column(String, nullable=True)
 
-    auth_provider = Column(String, default="local")
+    auth_provider = Column(Enum(AuthProvider), default=AuthProvider.local, nullable=False)
     provider_user_id = Column(String, nullable=True) 
 
     is_active = Column(Boolean, nullable=False, default=True)
